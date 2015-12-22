b94a1777dff972e3fcc2f40a5d663e64f5fec0a2
sweetvvck <sweetvvck@gmail.com>
2015-12-22 20:26:16 +0800

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# node-greenhat
+# Node Greenhat
 :construction_worker: Quick hack for making real work happen.
+
+Make your github `Contributions` look awesome. This is inspired by [Greenhat](https://github.com/4148/greenhat)
+
+# Usage
+
+```
+git clone git@github.com:sweetvvck/node-greenhat.git
+
+(sudo) npm i
+
+node greenhat.js days fromDate
+
+#days means how many days you want to fill your contributions
+#fromDate means from when you want to fill your contributions
+```
---
+++ greenhat.js
@@ -0,0 +1,66 @@
+var async = require('async');
+var exec= require('child_process').exec;
+
+
+//Sat Dec 12 00:00:00 2015  -0400
+function getDateStr (n, startDate) {
+	var d = startDate.setDate(startDate.getDate() - n);
+	var dStr = startDate.toString();
+	dArr = dStr.split(' ').slice(0, 5);
+	var tmp;
+	tmp = dArr[3];
+	dArr[3] = dArr[4];
+	dArr[4] = tmp;
+	return dArr.join(' ') + ' -0400';
+}
+
+function handleOutput(err, stdout, stderr) {
+	if(err) {
+		console.error('An error occurred: ', err);
+	}
+	if(stdout) {
+		console.log(stdout);
+	}
+	if(stderr) {
+		console.error(stderr);
+	}
+}
+
+function main() {
+
+  var options = process.argv.slice(2);
+  var n, startdate;
+  if(options.length < 1 || options.length > 2) {
+    console.error("Bad params length");
+    return;
+  }
+  n = parseInt(options[0]);
+  startdate = options.length > 1 ? new Date(options[1]) : new Date()
+	console.log('start');
+	async.timesSeries(n, function (i, next) {
+		var curdate = getDateStr(i, startdate)
+		var numCommits = Math.floor((Math.random() * 10) + 1);
+		async.timesSeries(numCommits, function (index, cb) {
+			commit(curdate, cb);
+		}, next)
+	}, function () {
+		console.log('done');
+		exec("git rm realwork.txt; git commit -m 'delete'; git push;" , handleOutput);
+	});
+}
+
+function commit(curdate, callback) {
+	exec("echo '"
+		+ curdate
+		+ Math.floor((Math.random() * 1000000) + 1)
+		+ "' > realwork.txt; git add realwork.txt; GIT_AUTHOR_DATE='"
+		+ curdate
+		+ "' GIT_COMMITTER_DATE='"
+		+ curdate
+		+ "' git commit -m 'update'; git push;", function (err, stdout, stderr) {
+			handleOutput(err, stdout, stderr);
+			callback();
+		});
+}
+
+main();
